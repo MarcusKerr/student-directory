ce9b8f004191b2adf097f3e3eeb61339a3cf520d
Marcus <marcus.kerr94@gmail.com>
2018-07-04 18:52:39 +0100

    Changed specific letter method, modified program to be able to output students with a name less than 12 characters

--- directory.rb
+++ directory.rb
@@ -28,38 +28,54 @@ end
 
 # Print pref method
 # Ask user how they would like to print the directory
-def print_pref
+def print_pref(students)
   puts "How would you like to print the directory ('by letter'/'less than 12'/'all')?"
   print_pref = gets.chomp
+  if print_pref == "all"
+    print(students)
+  elsif print_pref == "by letter"
+    print_by_letter(students)
+  elsif print_pref == "less than 12"
+    less_than_12(students)
+  else
+    print_pref(students)
+  end
 end
+
 # Only print students whos name begins with
 # Specific letter
-def print_by_letter
+def print_by_letter(students)
     puts "Enter first letter of name"
     letter = gets.chomp
+
+    students_by_letter = []
+    students.each do |student|
+      if letter == student[:name][0]
+        students_by_letter << student
+      end
+    end
+    print(students_by_letter)
+end
+
+# Only print students whos name is less
+# than 12 characters
+def less_than_12(students)
+    name_less_than_12 = []
+    students.each do |student|
+      if student[:name].length < 12
+        name_less_than_12 << student
+      end
+    end
+    print(name_less_than_12)
 end
 
 # Method to print students
 # Loop to print each student in the students
 # According to print pref result
 def print(students)
-    print_preference = print_pref
-    if print_preference == "all"
-      students.each_with_index do |student, index|
-        puts "#{index+ 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-      end
-
-    # If preint preference is by letter
-    # Ask user to enter a letter
-    # Print all studnets with name beginning with entered letter
-    elsif print_preference == "by letter"
-      letter = print_by_letter
-      students.each_with_index do |student, index|
-        if student[:name][0] == letter
-          puts "#{index+ 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-        end
-      end
-    end
+  students.each_with_index do |student, index|
+    puts "#{index+ 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
 end
 
 # Footer method
@@ -71,5 +87,5 @@ end
 # Call methods
 students = input_students
 print_header
-print(students)
+print_pref(students)
 print_footer(students)
