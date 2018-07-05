33d7c0d30faa3c9e6d54e33b4c81258e9f1da350
Marcus <marcus.kerr94@gmail.com>
2018-07-05 18:51:06 +0100

    Used hash to print by cohort

--- directory.rb
+++ directory.rb
@@ -3,18 +3,55 @@
 # And creating student array
 def input_students
   puts "Please enter the names of the students"
-  puts "To finish, hit return twice"
+  puts "To finish, leave name empty"
+
   # Create an empty array
   students = []
-  # Get the first name
-  name = gets.chomp
-  # While the name is not empty repead this code
-  while !name.empty? do
-    # Add the student hash to the array
-    students << {name: name, cohort: :november, fav_sport: "That sport", fav_food: "That food"}
-    puts "Now we have #{students.count} students"
+  months = [
+    :January,
+    :February,
+    :March,
+    :April,
+    :May,
+    :June,
+    :July,
+    :August,
+    :September,
+    :October,
+    :November,
+    :December
+  ]
+
+  # # Get the first name
+  # puts "Enter a name"
+  # name = gets.chomp
+  #
+  # # Get the first cohort
+  # puts "Enter cohort"
+  # cohort = gets.chomp.to_sym
+
+  # While the name is not empty repeat this code
+  while true do
+
     # Get another name from the user
+    puts "Enter a name"
     name = gets.chomp
+    if name.empty?
+      break
+    end
+
+    # Get another cohort from user
+    puts "Enter cohort"
+    cohort = gets.chomp.capitalize.to_sym
+
+    if !(months.include?(cohort))
+      cohort = " -".to_sym
+    end
+
+    # Add the student hash to the array
+    students << {name: name, cohort: cohort, fav_sport: "That sport", fav_food: "That food"}
+    puts "Now we have #{students.count} students"
+
   end
   # Return the array of students
   students
@@ -22,14 +59,14 @@ end
 
 # Header method
 def print_header
-  puts "The students of Villains Academy".center(100)
-  puts "-------------".center(100)
+  puts "The students of Villains Academy".center(75)
+  puts "-------------".center(75)
 end
 
 # Print pref method
 # Ask user how they would like to print the directory
 def print_pref(students)
-  puts "How would you like to print the directory ('by letter'/'less than 12'/'all')?".center(100)
+  puts "How would you like to print the directory ('by letter'/'less than 12'/'by cohort'/'all')?".center(75)
   print_pref = gets.chomp
   if print_pref == "all"
     print(students)
@@ -37,6 +74,8 @@ def print_pref(students)
     print_by_letter(students)
   elsif print_pref == "less than 12"
     less_than_12(students)
+  elsif print_pref == "by cohort"
+    by_cohort(students)
   else
     print_pref(students)
   end
@@ -69,6 +108,19 @@ def less_than_12(students)
     print(name_less_than_12)
 end
 
+# Print by cohort
+def by_cohort(students)
+  puts "Enter cohort you would like to view"
+  cohort = gets.chomp.capitalize.to_sym
+  students_by_cohort = []
+  students.map { |student|
+    if student[:cohort] == cohort
+      students_by_cohort << student
+    end
+  }
+  print(students_by_cohort)
+end
+
 # Method to print students
 # Loop to print each student in the students
 # According to print pref result
@@ -76,7 +128,7 @@ def print(students)
 
   index = 0
   until index == students.length
-    puts "#{index + 1}. #{students[index][:name]} (#{students[index][:cohort]} cohort) #{students[index][:fav_food]}".center(100)
+    puts "#{index + 1}. #{students[index][:name]} (#{students[index][:cohort]} cohort) #{students[index][:fav_food]}".center(75)
     index += 1
   end
 
@@ -88,7 +140,7 @@ end
 # Footer method
 def print_footer(names)
   # Print total number of students
-  puts "Overall we, have #{names.count} great students".center(100)
+  puts "Overall we, have #{names.count} great students".center(75)
 end
 
 # Call methods
